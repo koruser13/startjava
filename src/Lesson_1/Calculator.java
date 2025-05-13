155f2c3c4c2d7aa5ab01f37b8e852eda1f0bef85
koruser13 <kit76tam@gmail.com>
2025-05-13 17:15:22 +0300

    Внес изменения

--- src/Lesson_1/Calculator.java
+++ src/Lesson_1/Calculator.java
@@ -1,21 +1,21 @@
 public class Calculator {
     public static void main(String[] args) {
         int firstNum = 2;
-        String sign = "^";
-        int secondNum = 10;
+        char sign = '^';
+        int secondNum = 3;
         int result = 1;
 
-        if (sign.equals("+")) {
+        if (sign == '+') {
             result = firstNum + secondNum;
-        } else if (sign.equals("-")) {
+        } else if (sign == '-') {
             result = firstNum - secondNum;
-        } else if (sign.equals("*")) {
+        } else if (sign == '*') {
             result = firstNum * secondNum;
-        } else if (sign.equals("/")) {
+        } else if (sign == '/') {
             result = firstNum / secondNum;
-        } else if (sign.equals("%")) {
+        } else if (sign == '%') {
             result = firstNum % secondNum;
-        } else if (sign.equals("^")) {
+        } else if (sign == '^') {
             for (int i = secondNum; i > 0; i--) {
                 result *= firstNum;
             }
